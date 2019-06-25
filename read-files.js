a820f557acd8204c182294e42679accd3b892019
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-25 14:25:45 -0700

    copy a file with fs.readFile then writeFile

--- read-files.js
+++ read-files.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 
 fs.readFile('./hi', { encoding: 'utf8' }, (err, data) => {
-  if(err) {
-    // handle erro reasonalby
-    console.error(err);
-    return;
-  }
+  if(err) return console.error(err);
   console.log(data);
+  fs.writeFile('./greeting', data, err => {
+    if(err) return console.error(err);
+  });
 });
